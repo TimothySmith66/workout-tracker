fe1165356519a19b93ce016d704046db43ec9c51
Timothy Smith <timothytsmith8@gmail.com>
2021-02-08 20:41:41 -0800

    Connected to express.

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose');
 
+// express dependency
+const express = require('express');
+ //handle bars dependendency
+// calls express 
+ const app = express();
 
+ const PORT = process.env.PORT || 3001;
+
+app.use(express.static("public"));
+// For: data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"))
 
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
@@ -11,3 +23,11 @@ mongoose.connect(
       useFindAndModify: false
     }
   );
+
+
+
+//port listener
+app.listen(PORT, () => {
+    console.log("App listening on PORT " + PORT);
+});
+
